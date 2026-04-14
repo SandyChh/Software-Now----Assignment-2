a5ea916a5475e395465fdd8a1dffe67f012e7f4f
Udbhav Ghimire <ghimireudbhav@gmail.com>
2026-04-14 22:26:11 +1000

    decryption and verification part added

--- question1/main.py
+++ question1/main.py
@@ -4,15 +4,22 @@ def get_file_data(path):    # Reads the content of a file and returns it as a st
     f.close()
     return data
 
-def gen_cipher(start, end, shift):  # Generates a cipher mapping for characters in the range from start to end with a given shift
+
+# Generates a cipher mapping for characters in the range from start to end with a given shift
+def gen_cipher(start, end, shift):
     diff = ord(end)-ord(start)+1
-    cipher = {chr(value+ord(start)): chr(((value+shift)%diff)+ord(start)) for value in range(diff)}
+    cipher = {chr(value+ord(start)): chr(((value+shift) % diff)+ord(start))
+              for value in range(diff)}
     return cipher
 
-def transform(text, mapping):   # Transforms the text using the provided mapping and leaves characters unchanged if they are not in the mapping.
+
+# Transforms the text using the provided mapping and leaves characters unchanged if they are not in the mapping.
+def transform(text, mapping):
     return ''.join(map(lambda x: mapping[x] if x in mapping else x, text))
 
-def encrypt(ip, op, shift1, shift2):    # Reads input file, generates ciphers, encrpyts the data using the ciphers, and writes the encrypted data to the output file.
+
+# Reads input file, generates ciphers, encrpyts the data using the ciphers, and writes the encrypted data to the output file.
+def encrypt(ip, op, shift1, shift2):
     data = get_file_data(ip)    # Getting data to encrypt from input file path
 
     # Generating ciphers for differenct character ranges based on the provided shift values.
@@ -21,18 +28,38 @@ def encrypt(ip, op, shift1, shift2):    # Reads input file, generates ciphers, e
     A_M_cipher = gen_cipher('A', 'M', -shift1)
     N_Z_cipher = gen_cipher('N', 'Z', shift2**2)
 
-    cipher = {**a_m_cipher, **n_z_cipher, **A_M_cipher, **N_Z_cipher}   # Combining all the generated ciphers into a single mapping for encryption.
+    # Combining all the generated ciphers into a single mapping for encryption.
+    cipher = {**a_m_cipher, **n_z_cipher, **A_M_cipher, **N_Z_cipher}
 
     encrypted_data = transform(data, cipher)
     with open(op, 'w') as file:
         file.write(encrypted_data)
-        
+
     return cipher   # Returning the cipher used for decryption purposed later on
+
+
+def decrypt(ip, op, cipher):
+    data = get_file_data(ip)
+    rev_cipher = {v: k for k, v in cipher.items()}
+    decrypted_data = transform(data, rev_cipher)
+    with open(op, 'w') as file:
+        file.write(decrypted_data)
+
+
+def verify(path1, path2):
+    file1 = get_file_data(path1)
+    file2 = get_file_data(path2)
+    if file1 == file2:
+        print('The decryption is successful.')
+    else:
+        print('The decryption is not successful')
+
 
 def get_input():    # Prompts the user to enter two shift values and validates the input to ensure they are integers.
     shift1 = int(input('Enter shift 1: '))  # Getting shift 1 value from user
     shift2 = int(input('Enter shift 2: '))  # Getting shift 2 value from user
     return shift1, shift2
+
 
 def main():
     shift1, shift2 = get_input()
